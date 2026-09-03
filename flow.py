0cbca007803c9894a263b50bfce64ca450cf28ca
Abjean407 <102196164+Abjean407@users.noreply.github.com>
2026-09-02 22:16:12 -0600

    Add flow control practice script

---
+++ flow.py
@@ -0,0 +1,28 @@
+print('Hey man, how old are you??')
+ 
+ 
+myAge = input()
+if myAge == '30':
+    print('Same here!')
+
+if myAge <= '30':
+    print('OH wow! I figured, did you play basketball? ')
+
+question=input()
+
+if question==('yes'):
+    print('I knew it!')
+
+if question==('no'):
+    print('I hope so youre so tall')
+
+
+print('what college did you go to?')
+
+college = input()
+
+if college == 'Uni':
+    print('Man did you go D1?!')
+
+else:
+    print('Thats dope you went to played ball in college')
